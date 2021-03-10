492cf7a16ee50fef461a3fce8a07858d2ed27dae
harman <harmanave@gmail.com>
2021-03-11 00:50:42 +0530

    removed pseudo code

--- src/SavedJobs.js
+++
@@ -1,5 +0,0 @@
-// SavedJobs.js
-
-// Have a button to save jobs for user on a button click event.
-// initilaize state of the button as false.
-// on clickEvent, update the state to true, save and display information in this component.
